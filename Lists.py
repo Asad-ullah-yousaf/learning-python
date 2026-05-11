0d2be51c369168422111387c4cadf5342b576497
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-05-11 16:12:09 +0500

    lists in python

---
+++ Lists.py
@@ -0,0 +1,14 @@
+# List : A list is an array in python that can be used to store multiple values, objects, strings or numbers
+
+numList = [1,2,3]
+print(numList)
+
+strList = ['Asad','Abdul','Ali']
+print(strList)
+
+mixedList = [1, 'Asad', 3.14, True]
+print(mixedList)
+
+objList = [1, 'Asad', 3.14, True, [1,2,3], {'name': 'Asad', 'age': 20}]
+print(objList)
+
