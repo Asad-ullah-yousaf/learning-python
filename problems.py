cd8a9edeb19d5a2705d0ba9d24af215a39e373aa
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-05-15 15:20:31 +0500

    add temperature converter and string manipulation examples

---
+++ problems.py
@@ -0,0 +1,28 @@
+# building a temperature converter that converts temperature from celsius to fahrenheit and vice versa.
+
+# temp = float(input("Enter the temperature you want to convert (e.g. 37): "))
+# unit = input("Enter the unit of the temperature you want to convert (C for celsius and F for fahrenheit): ").upper()
+
+# tempObj = { 'value':temp, 'unit':unit }
+# print(f"The temperature you entered is {tempObj['value']} {tempObj['unit']}")
+
+# if tempObj['unit'] in ('C'):
+#     C = tempObj['value']
+#     celcius = (C * 9/5) + 32
+#     print(f"The Temperature is converted from C to {celcius} F")
+# elif tempObj['unit'] in ('F'):
+#     F = tempObj['value']
+#     farenheit = (F - 32) * 5/9
+#     print(f"The temperature was converted from Farenheit to {farenheit} C")
+# else:
+#     print("Invalid Input")
+
+
+# String Manipulation : String Splicer and shifter
+# What to do: Take any string longer than 5 characters (e.g., "Python"). 
+# Print the first 3 characters, the last 2 characters, and then print the entire string completely reversed
+
+string = "Python"
+
+
+
