4cbdfe6fc991447fb3a227fc95e43b347363926f
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-05-18 15:24:02 +0500

    implement temperature converter and library fee calculator; add loops demonstration

---
+++ loops.py
@@ -0,0 +1,38 @@
+# Loops: Loops are used to repeat a block of code multiple times
+# To execute a repeatative task we use loops which are of two types:
+# 1. For Loop: A for loop is used to iterate over a sequence (like a list, tuple, or string) 
+# and execute a block of code for each item in the sequence.
+# 2. While Loop: A while loop is used to execute a block of code as long as a specified condition is true.
+
+# While Loop
+
+count = 0
+
+# while count <= 5:
+#     print(count)
+#     count += 1
+# else:
+#     print("Loop has ended")
+
+
+# using break in a while loop
+
+while count < 5:
+    if count == 3:
+        count += 1
+        continue
+    print(count)
+    count = count + 1
+    
+    
+
+
+# A for loop is used to iterate over a sequence (like a list, tuple, or string) 
+# and execute a block of code for each item in the sequence.
+language = 'Python'
+for letter in language:
+    print(letter)
+
+
+for i in range(len(language)):
+    print(language[i])
--- problems.py
+++ problems.py
@@ -1,21 +1,21 @@
 # building a temperature converter that converts temperature from celsius to fahrenheit and vice versa.
 
-# temp = float(input("Enter the temperature you want to convert (e.g. 37): "))
-# unit = input("Enter the unit of the temperature you want to convert (C for celsius and F for fahrenheit): ").upper()
+temp = float(input("Enter the temperature you want to convert (e.g. 37): "))
+unit = input("Enter the unit of the temperature you want to convert (C for celsius and F for fahrenheit): ").upper()
 
-# tempObj = { 'value':temp, 'unit':unit }
-# print(f"The temperature you entered is {tempObj['value']} {tempObj['unit']}")
+tempObj = { 'value':temp, 'unit':unit }
+print(f"The temperature you entered is {tempObj['value']} {tempObj['unit']}")
 
-# if tempObj['unit'] in ('C'):
-#     C = tempObj['value']
-#     celcius = (C * 9/5) + 32
-#     print(f"The Temperature is converted from C to {celcius} F")
-# elif tempObj['unit'] in ('F'):
-#     F = tempObj['value']
-#     farenheit = (F - 32) * 5/9
-#     print(f"The temperature was converted from Farenheit to {farenheit} C")
-# else:
-#     print("Invalid Input")
+if tempObj['unit'] in ('C'):
+    C = tempObj['value']
+    celcius = (C * 9/5) + 32
+    print(f"The Temperature is converted from C to {celcius} F")
+elif tempObj['unit'] in ('F'):
+    F = tempObj['value']
+    farenheit = (F - 32) * 5/9
+    print(f"The temperature was converted from Farenheit to {farenheit} C")
+else:
+    print("Invalid Input")
 
 
 # String Manipulation : String Splicer and shifter
@@ -28,19 +28,19 @@
 
 # Problem 3: The Library Fee Calculator
 
-# days_late = int(input("Enter the number of days late: "))
-# fine_amount = 0;
+days_late = int(input("Enter the number of days late: "))
+fine_amount = 0;
 
-# if days_late <= 5 and days_late != 0:
-#     total_fine = fine_amount + days_late
-#     print(f"you returned book {days_late} days late and your total fine will be $ {total_fine}")
-# elif days_late >= 6 :
-#     total_fine = (fine_amount + days_late) * 2
-#     print(f"you returned book {days_late} days late and your total fine will be $ {total_fine}")
-# elif days_late < 1:
-#     print("Please enter a valid input")
-# else:
-#     print("Please enter a valid input")
+if days_late <= 5 and days_late != 0:
+    total_fine = fine_amount + days_late
+    print(f"you returned book {days_late} days late and your total fine will be $ {total_fine}")
+elif days_late >= 6 :
+    total_fine = (fine_amount + days_late) * 2
+    print(f"you returned book {days_late} days late and your total fine will be $ {total_fine}")
+elif days_late < 1:
+    print("Please enter a valid input")
+else:
+    print("Please enter a valid input")
     
     
     
@@ -51,20 +51,20 @@
 #         Remove the youngest age (15) from the list.
 #         Bonus challenge: Create a new empty list called adults. Loop through your ages list, and if an age is 18 or older, append it to the adults list.
 
-# ages = [16, 21, 18, 30, 15, 25]
-# ages.append(22)
-# print(ages)
+ages = [16, 21, 18, 30, 15, 25]
+ages.append(22)
+print(ages)
 
 
-# ages.remove(min(ages))
-# print(ages)
+ages.remove(min(ages))
+print(ages)
 
-# adults = []
+adults = []
 
-# for i in ages:
-#     if i >= 18:
-#         adults.append(i)
-# print(adults)
+for i in ages:
+    if i >= 18:
+        adults.append(i)
+print(adults)
 
 
 # Problem 5: Coordinates (Tuple Exploration)
@@ -73,13 +73,13 @@
 #         Try to change the first value to 5 and observe the error Python gives you.
 #         Unpack the tuple into two separate variables, x and y, and print them.
 
-# point = (4, 10)
-# x = point[0]
-# y = point[1]
+point = (4, 10)
+x = point[0]
+y = point[1]
 
-# point[0] = 5
-# print(x)
-# print(y)
+point[0] = 5
+print(x)
+print(y)
 
 
 
