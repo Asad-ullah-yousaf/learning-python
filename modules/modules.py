6ae28fc42f0643cac3f026dd380bbaa114edf2cb
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-05-19 13:03:13 +0500

    created a function that generates random number of ids with random number of chars based on user input

--- modules/modules.py
+++ modules/modules.py
@@ -12,32 +12,55 @@ import string
 import math
 import random
 
-str = string.ascii_letters
-# print(str)
+# str = string.ascii_letters
+# # print(str)
 
-number = string.digits
-# print(number)
+# number = string.digits
+# # print(number)
 
-combination = str + number
-# print(combination)
+# combination = str + number
+# # print(combination)
 
-list_of_uids = []
+# list_of_uids = []
 
-def generate_random_uid(combination):
-    id = []
-    while len(id) < 6:
-       ran = random.randint(0, 61)
-       id.append(combination[ran])
-    print("Random Id",id)
+# def generate_random_uid(combination):
+#     id = []
+#     while len(id) < 6:
+#        ran = random.randint(0, 61)
+#        id.append(combination[ran])
+#     print("Random Id",id)
    
-    uid = ''.join(id)
-    print("Random generated UId",uid)
-    list_of_uids.append(uid)
+#     uid = ''.join(id)
+#     print("Random generated UId",uid)
+#     list_of_uids.append(uid)
     
     
      
-for i in range(5):
-    print('---------')
-    generate_random_uid(combination)
+# for i in range(5):
+#     print('---------')
+#     generate_random_uid(combination)
 
-print(list_of_uids)
+# print(list_of_uids)
+
+num_of_ids = int(input("How many ids to generate: "))
+lst_of_ids = []
+def generate_user_id():
+    char_of_id = int(input("Number of characters the id should have: "))
+    
+    
+    comb = string.ascii_letters + string.digits
+    ids = []
+    while len(ids) < char_of_id:
+        random_num = random.randint(0,61)
+        ids.append(comb[random_num])
+    print(ids)
+    new = ''.join(ids)
+    lst_of_ids.append(new)
+    
+        
+        
+for i in range(num_of_ids):
+    generate_user_id()
+    print('-------')
+    
+print(lst_of_ids)
