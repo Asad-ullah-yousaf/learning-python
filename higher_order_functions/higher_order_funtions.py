a69669f7ee5d85bbf86b23f8f7ba8c31a39e417b
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-06-16 15:36:11 +0500

    Add exercises demonstrating map and filter functions with countries, names, and numbers

--- higher_order_functions/higher_order_funtions.py
+++ higher_order_functions/higher_order_funtions.py
@@ -91,3 +91,45 @@ def is_even(num):
 
 check_num = filter(is_even, numbers)
 print(list(check_num))
+
+
+# Exercises
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in countries:
+    print(i.upper())
+print("-------------------")
+for i in names:
+    print(i)
+print("-------------------")
+for i in numbers:
+    print(i)
+
+
+# Use map to create a new list by changing each country to uppercase in the countries list
+map_countries = map(lambda x:x.upper(), countries)
+print(list(map_countries))
+# Use map to create a new list by changing each number to its square in the numbers list
+map_numbers = map(lambda i: i**2, numbers)
+print(list(map_numbers))
+# Use map to change each name to uppercase in the names list
+map_names = map(lambda n:n.upper(), names)
+print('Name to uppercase',list(map_names))
+
+# Use filter to filter out countries containing 'land'.
+filter_countries = filter(lambda c:'land' in c, countries)
+print('Countries having Land in their names',list(filter_countries))
+# Use filter to filter out countries having exactly six characters.
+filter_based_on_char = filter(lambda c: len(c) == 6, countries)
+print('Countries having 6 characters',list(filter_based_on_char))
+# Use filter to filter out countries containing six letters and more in the country list.
+filter_chars = filter(lambda c: len(c)>6,countries)
+print('Having more than 6 chars',list(filter_chars))
+# Use filter to filter out countries starting with an 'E'
+filter_chars_initial = filter(lambda c: c.startswith('E'),countries)
+print('Starts with E:',list(filter_chars_initial))
+# Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+# Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+# Use reduce to sum all the numbers in the numbers list.
