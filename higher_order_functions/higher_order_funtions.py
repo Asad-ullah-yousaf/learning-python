0fa929c83a8520763b578f5be8ce27933d5c1cee
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-06-16 12:36:02 +0500

    map functions

--- higher_order_functions/higher_order_funtions.py
+++ higher_order_functions/higher_order_funtions.py
@@ -1,59 +1,81 @@
-# Higher Order functions:
-# A higher order function is a function that takes another function as an argument, or returns a function as a result
+# # Higher Order functions:
+# # A higher order function is a function that takes another function as an argument, or returns a function as a result
 
-import math
-
-
-def add_nums(num):
-    return sum(num)
-
-numbers = [1, 72, 3, 94, 55]
-# print(add_nums(numbers))
-
-def square_nums(num):
-    return math.floor(math.pow(num, 2))
+# import math
 
 
-# print(square_nums(283))
+# def add_nums(num):
+#     return sum(num)
 
-def higher_od_func(f,lst):
-    result = f(lst)
+# numbers = [1, 72, 3, 94, 55]
+# # print(add_nums(numbers))
+
+# def square_nums(num):
+#     return math.floor(math.pow(num, 2))
+
+
+# # print(square_nums(283))
+
+# def higher_od_func(f,lst):
+#     result = f(lst)
+#     return result
+
+# test_func = higher_od_func(add_nums,numbers)
+
+# print(test_func)
+
+
+# # closures : A closure is a function object that has access to variables in its enclosing scope, 
+# # even after the outer function has finished executing.
+
+# def outer_func(x):
+#     def inner_func(y):
+#         return x + y
+#     return inner_func
+
+# add_number = outer_func(524)
+# print(add_number(5))
+
+
+# arr = [1,2,3,4]
+
+# for i in arr:
+#     print(f".{i}")
+
+# def sum_numbers(num):
+#     result = 0
+#     for i in arr :
+#         result += i
+#     return result
+
+
+# print(sum_numbers(arr))
+
+# def higher_order_function(f, num):
+#     result = f(num)
+#     return result ** 2
+
+
+# print(higher_order_function(sum_numbers,arr))
+
+# MAP FUNCTIONS
+arr = [1,2,3,4,5]
+
+def square(nums):
+    result = nums ** 2
     return result
+    
 
-test_func = higher_od_func(add_nums,numbers)
-
-print(test_func)
-
-
-# closures : A closure is a function object that has access to variables in its enclosing scope, 
-# even after the outer function has finished executing.
-
-def outer_func(x):
-    def inner_func(y):
-        return x + y
-    return inner_func
-
-add_number = outer_func(524)
-print(add_number(5))
+mapped_func = map(square,arr)
+print(list(mapped_func))
 
 
-arr = [1,2,3,4]
+text = 'python'
 
-for i in arr:
-    print(f".{i}")
+def uppercase(text):
+    result = text.upper()
+    return result;
 
-def sum_numbers(num):
-    result = 0
-    for i in arr :
-        result += i
-    return result
+map_text = map(uppercase, text)
 
-
-print(sum_numbers(arr))
-
-def higher_order_function(f, num):
-    result = f(num)
-    return result ** 2
-
-
-print(higher_order_function(sum_numbers,arr))
+print(list(map_text))
