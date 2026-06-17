6e5f7b9863e7ca2374fb27ae2e0ec83d60879b70
Asad-ullah-yousaf <asadullahyousaf909@gmail.com>
2026-06-17 15:03:19 +0500

    Add examples for extended unpacking, spreading, and enumerate functions

--- exceptional_handling.py
+++ exceptional_handling.py
@@ -31,3 +31,37 @@ def unpacking_person_info(name, country, city, age):
     return f'{name} lives in {country}, {city}. He is {age} year old.'
 dct = {'name':'Asabeneh', 'country':'Finland', 'city':'Helsinki', 'age':250}
 print(unpacking_person_info(**dct)) # Asabeneh lives in Finland, Helsinki. He is 250 years old.
+
+
+
+# Extended Unpacking
+countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+fin, sw, nor, *rest = countries
+print(fin, sw, nor, rest)   # Finland Sweden Norway ['Denmark', 'Iceland']
+numbers = [1, 2, 3, 4, 5, 6, 7]
+one, *middle, last = numbers
+print(one, middle, last)      #  1 [2, 3, 4, 5, 6] 7
+
+cities = ['NewYork','LosAngles','Chicago','Washington']
+new,los,*rest = cities
+print(new,los,rest)
+
+
+# Spreading in python
+lst_one = [1,3,5,7,9]
+lst_two = [2,4,6,8]
+lst = [*lst_one, *lst_two]
+print(lst)
+
+# Enumerate
+lst = [1,2,3,4]
+
+for index,i in enumerate(lst):
+    print(index,i)
+
+
+countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+for index, i in enumerate(countries):
+    if i == 'Norway':
+        print(f"{i} is at {index} index in the Array")
+    print(index,i)
